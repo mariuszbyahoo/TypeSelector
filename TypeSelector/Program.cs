1f26827a3ac1fde21996e31c20d104eadac0336b
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2020-02-11 11:58:49 +0100

    Add project files.

---
+++ TypeSelector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TypeSelector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Here will be method invocation and method name selection
+        }
+    }
+}
